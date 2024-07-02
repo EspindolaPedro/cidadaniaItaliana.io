272c25e38b11990161828fe19d21a600bbd8ee86
Pedro <pedro.paulo.espindola2710@gmail.com>
2024-07-02 18:52:26 -0400

    teste slide 1.0

--- assets/script.js
+++ assets/script.js
@@ -25,34 +25,39 @@ function nextImage() {
     document.getElementById("radio"+count).checked = true;
 }
 
-const slider = document.querySelector('.slider');
+const slider = document.querySelector('.slides');
 const slides = document.querySelectorAll('.slide');
 let startX, endX, diffX;
 let currentIndex = 0;
 
 const handleTouchStart = (e) => {
-    startX = e.touches[e].clientX;
+  startX = e.touches[0].clientX;
 };
 
 const handleTouchMove = (e) => {
-    endX = e.touches[0].clientX
-}
-
-const handleTouchEnd = (e) => {
-    diffX = startX - endX;
-    if (diffX > 50) {
-        currentIndex - Math.min(currentIndex + 1, slides.length - 1);
-    } else if (diffX < -50) {
-        currentIndex = Math.max(currentIndex - 1, 0)
-    }
-    updateSlider();
+  endX = e.touches[0].clientX;
 };
 
-const updateSlider = (e) => {
-    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+const handleTouchEnd = () => {
+  diffX = startX - endX;
+  if (diffX > 50) {
+    // Deslizar para a esquerda
+    currentIndex = Math.min(currentIndex + 1, slides.length - 1);
+  } else if (diffX < -50) {
+    // Deslizar para a direita
+    currentIndex = Math.max(currentIndex - 1, 0);
+  }
+  updateSlider();
 };
- 
 
+const updateSlider = () => {
+  const radioButtons = document.querySelectorAll('input[name="radio-btn"]');
+  if (radioButtons[currentIndex]) {
+    radioButtons[currentIndex].checked = true;
+  }
+};
+
+// Adiciona os eventos de toque ao contêiner do slider
 slider.addEventListener('touchstart', handleTouchStart);
 slider.addEventListener('touchmove', handleTouchMove);
 slider.addEventListener('touchend', handleTouchEnd);
